3a1220d5f8246854ea0086ba81317a9a2e1fa4df
Irwan Setiawan <i.setiawan@criteo.com>
2020-10-29 16:20:16 +0800

    Swift loadBid enrichAdObject and bid.price

--- SwiftTest/SwiftTest/ViewController.swift
+++ SwiftTest/SwiftTest/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import CriteoPublisherSdk
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+  }
+
+  func testLoadBidWithNilCheck() {
+
+    let adUnit = CRBannerAdUnit.init(adUnitId: "", size: CGSize(width: 320, height: 50))
+
+    Criteo.shared().loadBid(for: adUnit) { maybeBid in
+        // Called when a response is received, or timed out.
+        // Run your auction here
+        var criteoBid : NSMutableDictionary = [:]
+
+        if let bid = maybeBid {
+            // add Criteo bids into Ad Manager request
+            Criteo.shared().enrichAdObject(criteoBid, with: bid)
+
+            let price = bid.price
+        }
+
+    }
   }
 
 
