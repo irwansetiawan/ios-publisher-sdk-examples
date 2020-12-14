db8640beebe3af79bbef8127c524a97e90056820
Irwan Setiawan <i.setiawan@criteo.com>
2020-12-14 10:33:25 +0800

    Admob: Add empty project

---
+++ Mediation-Admob/Mediation-Admob/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Mediation-Admob
+//
+//  Created by Irwan Setiawan on 14/12/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
